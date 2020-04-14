043c1ed14b56ff948c89992d63404e62b231cf06
Tasit Sappooree <jetdokoalah@gmail.com>
2020-04-14 15:27:01 +0700

    Initial project

--- app/src/main/java/com/cs/tu/caruserapp/StartActivity.java
+++ app/src/main/java/com/cs/tu/caruserapp/StartActivity.java
@@ -10,6 +10,7 @@ import android.widget.Button;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+//Test
 public class StartActivity extends AppCompatActivity {
 
     Button login, register;
